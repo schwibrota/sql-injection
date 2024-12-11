3ac35f0fee02350ef2525fe306d14495f230c7b1
noah.fedele@student.htl-rankweil.at <noah.fedele@student.htl-rankweil.at>
2024-12-11 10:34:39 +0000

    Edited LoggedInPage and InputPage

--- client_code/InputPage/__init__.py
+++ client_code/InputPage/__init__.py
@@ -15,33 +15,43 @@ class InputPage(InputPageTemplate):
     if ("checkboxChecked" in properties):
       if (properties["checkboxChecked"]):
         self.check_box_lvl1.checked = "true"
+    self.level = 1
+    if (self.check_box_lvl1.checked == "true"):
+      self.level = 2
     
   def button_anmelden_click(self, **event_args):
-    userdata = anvil.server.call('get_account_information', self.text_box_username.text, self.text_box_pw.text)
-    # test input
-    sonderzeichen_muster = r"['-]"
-    if (self.check_box_sql_inject.checked):
-      if (bool(re.search(sonderzeichen_muster, self.text_box_pw.text))):
-        anzuzeigendertext = "Hallo, Räuber! Kein Zugriff!"
-        pass
-      elif (bool(re.search(sonderzeichen_muster, self.text_box_username.text))):
-        anzuzeigendertext = "Hallo, Räuber! Kein Zugriff!"
-        pass
+    if (self.level == 1):
+      userdata = anvil.server.call('get_account_information', self.text_box_username.text, self.text_box_pw.text)
+      # test input
+      sonderzeichen_muster = r"['-]"
+      if (self.check_box_sql_inject.checked):
+        if (bool(re.search(sonderzeichen_muster, self.text_box_pw.text))):
+          anzuzeigendertext = "Hallo, Räuber! Kein Zugriff!"
+          pass
+        elif (bool(re.search(sonderzeichen_muster, self.text_box_username.text))):
+          anzuzeigendertext = "Hallo, Räuber! Kein Zugriff!"
+          pass
+        else:
+          print(f"Userdata: {userdata}")
+          anzuzeigendertext = f"Hallo, {userdata[0][1]}! Dein Kontostand beträgt {userdata[0][3]}\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
+          pass
       else:
-        print(f"Userdata: {userdata}")
-        anzuzeigendertext = f"Hallo, {userdata[0][1]}! Dein Kontostand beträgt {userdata[0][3]}\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
-        pass
+        if (len(userdata) == 1):    
+          print(f"Userdata: {userdata}")
+          anzuzeigendertext = f"Hallo, {userdata[0][1]}! Dein Kontostand beträgt {userdata[0][3]}\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
+          pass
+        elif (len(userdata) < 1):
+          anzuzeigendertext = f"User nicht vorhanden!\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
+        else:
+          anzuzeigendertext = f"Logged in, but no AccountNo passed\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
+          
+      loggedIn = LoggedInPage()
+      loggedIn.label_ausgabe.text = anzuzeigendertext
+      open_form(loggedIn, level1ready=True)
+
+    elif (self.level == 2):
+      url = f"?user={self.text_box_username.text}#pw={self.text_box_pw.text}"
+      pass
     else:
-      if (len(userdata) == 1):    
-        print(f"Userdata: {userdata}")
-        anzuzeigendertext = f"Hallo, {userdata[0][1]}! Dein Kontostand beträgt {userdata[0][3]}\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
-        pass
-      elif (len(userdata) < 1):
-        anzuzeigendertext = f"User nicht vorhanden!\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
-      else:
-        anzuzeigendertext = f"Logged in, but no AccountNo passed\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '{self.text_box_username.text}' AND Users.password = '{self.text_box_pw.text}';"
-        
-    loggedIn = LoggedInPage()
-    loggedIn.label_ausgabe.text = anzuzeigendertext
-    open_form(loggedIn)
+      pass
     
--- client_code/LoggedInPage/__init__.py
+++ client_code/LoggedInPage/__init__.py
@@ -10,9 +10,12 @@ class LoggedInPage(LoggedInPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
 
   def button_back_click(self, **event_args):
-    open_form("InputPage", checkboxChecked=True)
+    
+    if (self.label_ausgabe.text == "Logged in, but no AccountNo passed\nSELECT Users.AccountNo, Users.username, Users.password, Balances.balance FROM Users JOIN Balances ON Users.AccountNo = Balances.AccountNo WHERE Users.username = '' AND Users.password = '' OR '1'='1' --';"):
+      open_form("InputPage", checkboxChecked=True)
+    else:
+      open_form("InputPage", checkboxChecked=False)
     pass
