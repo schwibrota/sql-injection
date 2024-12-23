5735f191f10dbe8c4f4d89ba9f08bf3a175e956c
noah.fedele@student.htl-rankweil.at <noah.fedele@student.htl-rankweil.at>
2024-12-23 13:16:03 +0000

    Created Package1; Edited settings and LoggedInPage

--- client_code/LoggedInPage/__init__.py
+++ client_code/LoggedInPage/__init__.py
@@ -6,6 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
+
 class LoggedInPage(LoggedInPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
